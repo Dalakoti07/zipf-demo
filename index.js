e3ffdfbb46b67cd43a4ab49b127d9fd1174f67ec
lonewolf <saurabhdalakoti07@gmail.com>
2020-11-20 01:30:51 +0530

    chart generated

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 $('#btnCalculate').click(()=>{
     let text=$('#textArea').val();
 
-    let chars=text.split('');
+    allWords= getWords(text);
+    let wordCount= getWordsCount(allWords);
+    let sortedWordCount= sortWordCount(wordCount);
+    //console.log(sortedWordCount);
+    showDataInTable(sortedWordCount);
+    generateChart(sortedWordCount);
+})
+
+
+function getWords(inputText){
+    let chars=inputText.split('');
     let newChars=[];
     let count=0;
     chars.forEach((c) => {
         count++;
         switch(c){
+            case `  `: case `   `:
             case `!`: case `’`:
-            case `?`: 
+            case `?`: case `#`:
             case `'`: case `”`: case `“`:
             case `.`:
             case `-`: case `,`: case `"`:
@@ -21,6 +32,61 @@ $('#btnCalculate').click(()=>{
     });
 
     let newText = newChars.join('');
+    // console.log(newText)
     let allWords= newText.split(' ');
-    console.log(allWords);
-})
+    return allWords;
+}
+
+function getWordsCount(words){
+    let wordCount={};
+    words.forEach((word)=>{
+        if(wordCount[word]){
+            wordCount[word]++;
+        }else{
+            wordCount[word]=1;
+        }
+    })
+    return wordCount;
+}
+
+function sortWordCount(wordCounts){
+    let wordCountArray=[];
+    Object.keys(wordCounts).forEach((w)=>{
+        if(w=="")
+            return;
+        wordCountArray.push({
+            word:w,
+            count: wordCounts[w]
+        })
+    })
+    return wordCountArray.sort((a,b)=> b.count-a.count).slice(0,50);
+}
+
+function showDataInTable(wordCounts){
+    let table= $('#table');
+    table.empty();
+    wordCounts.forEach((w)=>{
+        table.append($(`<tr>`)
+                .append($(`<td>`).text(w.word))
+                .append($(`<td>`).text(w.count))
+        )
+    })
+}
+
+function generateChart(wordFrequency){
+    let ctx = document.getElementById('chart-canvas').getContext('2d');
+
+    let chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: wordFrequency.map((w)=> w.word),
+            datasets: [{
+              label: 'Word Freq',
+              borderColor: 'rgb(255, 99, 132)',
+              data: wordFrequency.map((w)=> w.count)
+            }]
+        }
+    });
+
+    // let idealChart= new Chart()
+}
